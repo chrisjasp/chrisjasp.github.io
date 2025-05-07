0ef87f7ab4516ed5dc23b32fa0c949119d65cb3b
Chris Jasper <chrisjasp@gmail.com>
2025-05-07 14:03:38 -0700

    moved assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,8 @@
     import type { LayoutProps } from "./$types";
     import 'bootstrap-icons/font/bootstrap-icons.min.css'
     import "../../assets/scss/theme.scss"
-    import { onMount } from "svelte";
 
     let { children }: LayoutProps = $props();
-
-    onMount(async () => {
-        await import("../../assets/js/vendors/color-modes");
-        await import("bootstrap/dist/js/bootstrap.bundle.min");
-        await import("../../assets/js/vendors/offcanvas.js");
-        await import("../../assets/js/vendors/navbar");
-        // await import("swiper")
-        await import("../../assets/js/vendors/swiper.js");
-        // await import("scrollcue");
-        await import("../../assets/js/vendors/scrollcue.js");
-    })
 </script>
 
 {@render children()}
