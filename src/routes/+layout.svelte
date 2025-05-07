ce3cd6b7cc53170b8b7ee39252dc1879477b9c97
Chris Jasper <chrisjasp@gmail.com>
2025-05-07 11:20:15 -0700

    image resources retry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
     import type { LayoutProps } from "./$types";
-    import '../../assets/js/vendors/color-modes'
     import 'bootstrap-icons/font/bootstrap-icons.min.css'
     import "../../assets/scss/theme.scss"
-
-    import 'bootstrap/dist/js/bootstrap.bundle.min'
-    import '../../assets/js/vendors/offcanvas.js'
-    import '../../assets/js/vendors/navbar'
-    import 'swiper'
-    import '../../assets/js/vendors/swiper.js'
-    import 'scrollcue'
-    import '../../assets/js/vendors/scrollcue.js'
+    import { onMount } from "svelte";
 
     let { children }: LayoutProps = $props();
+
+    onMount(async () => {
+        await import("../../assets/js/vendors/color-modes");
+        await import("bootstrap/dist/js/bootstrap.bundle.min");
+        await import("../../assets/js/vendors/offcanvas.js");
+        await import("../../assets/js/vendors/navbar");
+        // await import("swiper")
+        await import("../../assets/js/vendors/swiper.js");
+        // await import("scrollcue");
+        await import("../../assets/js/vendors/scrollcue.js");
+    })
 </script>
 
 {@render children()}
